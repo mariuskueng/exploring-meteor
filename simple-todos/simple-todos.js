955d042b3e868f62e9fe4cea8638e6052956d9ad
Marius Küng <apps@mariuskueng.ch>
2014-10-29 12:33:55 +0100

    Step 5 update and remove

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -26,6 +26,16 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+
+    "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+
+    "click .delete": function () {
+      Tasks.remove(this._id);
     }
+    
   });
 }
